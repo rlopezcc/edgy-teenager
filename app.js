b40b9ae73d4bf8fffef73edd8ba88bf4cb009405
rlopezcc <rlopez@sophilabs.com>
2015-08-21 12:23:15 -0300

    Use busboy instead of express.multipart, remove app.configure, add ejs version, read session secret from secrets.json

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var models = require('./models');
+var busboy = require('connect-busboy');
 var middlewares = require('./middlewares');
+var secrets = require('./secrets.json');
 var app = express();
 
-app.configure(function () {
-    app.use(express.favicon(__dirname + '/static/img/favicon.png'));
-    app.use(express.cookieParser());
+app.use(express.favicon(__dirname + '/static/img/favicon.png'));
+app.use(express.cookieParser());
+app.use(express.session({ secret: secrets.sessionKey }));
+app.set('views', __dirname + '/templates');
+app.use(express.bodyParser({uploadDir: './static/img/post-pics'}));
+app.use(busboy());
+app.use(express.static(__dirname + '/static'));
+app.use(function (req, res, next) {
+    // Make user accessible from template.
+    if (req.session.userId != null) {
+        models.User.findById(req.session.userId, function (err, user) {
+            if (!err) {
+                res.locals.user = user;
+                req.user = user;
+            }
+        });
+    }
+    next();
+});
+app.use(middlewares.addConfig);
+app.router = require('./routes')(app);
+app.engine('html', require('ejs').renderFile);
 
-    app.use(express.session({ secret: 'CEKRETO' }));
-    app.set('views', __dirname + '/templates');
-    app.use(express.bodyParser({uploadDir: './static/img/post-pics'}));
-    app.use(express.multipart());
-    app.use(express.static(__dirname + '/static'));
-    app.use(function (req, res, next) {
-        // Make user accessible from template.
-        if (req.session.userId != null) {
-            models.User.findById(req.session.userId, function (err, user) {
-                if (!err) {
-                    res.locals.user = user;
-                    req.user = user;
-                }
-            });
-        }
-        next();
-    });
-    app.use(middlewares.addConfig);
-    app.router = require('./routes')(app);
-    app.engine('html', require('ejs').renderFile);
-
-    app.use(function (req, res) {
-        res.status(404);
-        res.render('404.html', {url: req.url});
-    });
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404.html', {url: req.url});
 });
 
 app.listen(process.argv[2]);
